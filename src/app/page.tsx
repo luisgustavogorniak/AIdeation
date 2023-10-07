4d3a13aa2db9f14ae0d34c4503e24d03866e9850
luisgustavogorniak <gustavogorniak@gmail.com>
2023-10-07 20:13:57 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          AI note taking assistant.
+        </h1>
+      </div>
+    </div>
+  );
+}
